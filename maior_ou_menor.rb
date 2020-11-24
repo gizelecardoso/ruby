ba1c99e301c6010eae19b47b2e8cd2e970952be6
Gizele Santos <gizele.ccardoso@gmail.com>
2020-11-24 16:04:05 -0300

    Primeiro joguinho em Ruby - Acertando o numero secreto

---
+++ maior_ou_menor.rb
@@ -0,0 +1,146 @@
+def da_boas_vindas
+    puts
+    puts "        P  /_\\  P                              "
+    puts "       /_\\_|_|_/_\\                             "
+    puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+    puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!"
+    puts "  |' '  |  |_|  |'  ' |                        "
+    puts "  |_____| ' _ ' |_____|                        " 
+    puts "        \\__|_|__/                              "
+    puts
+    puts "Digite seu nome"
+    nome = gets.strip
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, #{nome}"
+    nome
+end
+
+def pede_dificuldade
+    puts "Qual o nível de dificuldade?"
+    puts "(1) Muito fácil (2) Fácil (3) Normal (4) Difícil (5) Impossível"
+    puts "Escolha: "
+    dificuldade = gets.to_i
+end
+
+def sorteia_numero_secreto (dificuldade)
+    case dificuldade 
+    when 1
+        maximo = 30
+    when 2
+        maximo = 60
+    when 3
+        maximo = 100
+    when 4
+        maximo = 150
+    else
+        maximo = 200
+    end
+
+    puts "Escolhendo um número secreto entre 1 e #{maximo - 1}..."
+    sorteado = rand(maximo) + 1 #variavel local
+
+    puts "Escolhido... que tal adivinhar hoje nosso número secreto"
+
+    #a ultima instrucao de uma funcao em ruby é o return e não precisa colocar a palavra return
+    sorteado
+end
+
+def pede_numero(chutes, tentativa, limite_de_tentativas)
+    puts "\n\n\n\n"
+    puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
+    puts "Chutes até agora #{chutes}"
+    puts "Entre com o número"
+    chute = gets.strip
+
+    puts "Será que acerto? Você chutou #{chute}"
+
+    chute.to_i
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
+
+    if acertou
+        ganhou
+        return true
+    end
+
+    maior = numero_secreto > chute
+    if maior
+        puts "O numero secreto é maior!"
+    else
+        puts "O numero secreto é menor!"
+    end
+    false
+end
+
+def joga(nome, dificuldade)
+    numero_secreto = sorteia_numero_secreto dificuldade #variavel global
+
+    pontos_ate_agora = 1000
+    limite_de_tentativas = 5
+
+    chutes = []
+    #total_de_chutes = 0
+    for tentativa in 1..limite_de_tentativas
+        chute = pede_numero chutes, tentativa, limite_de_tentativas
+
+        if nome == "Gizele"
+            ganhou
+            break
+        end
+
+        chutes << chute   # == chutes[total_de_chutes] = chute      == chutes[chutes.size] = chute
+        #o inverso do if = unless
+        pontos_a_perder = (chute - numero_secreto).abs / 2.0   #valor absoluto do numero - devolve o numero sem sinal
+        pontos_ate_agora -= pontos_a_perder
+
+        if verifica_se_acertou numero_secreto, chute
+            break
+        end
+    
+    end
+
+    puts "Você ganhou #{pontos_ate_agora} pontos. "
+end
+
+def ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+    puts "         OOOOOO         OOOOOO         "
+    puts "             OOOOOOOOOOOO              "
+    puts
+    puts "               Acertou!                "
+    puts
+end
+
+
+def nao_quer_jogar?
+    puts "Deseja jogar novamente? (S/N)"
+    quero_jogar = gets.strip
+    nao_quero_jogar = quero_jogar.upcase == 'N'
+end
+
+nome = da_boas_vindas
+
+dificuldade = pede_dificuldade
+
+loop do
+    joga nome, dificuldade
+
+    if nao_quer_jogar?  #? no final da funcao é que ela devolve um true or false
+        break
+    end
+end
